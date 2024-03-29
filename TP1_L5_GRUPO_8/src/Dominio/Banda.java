7b1c8a7efc1d9aa7e36df97bf88faf1afd62e102
Jose <84431245+jocmamorales@users.noreply.github.com>
2024-03-29 18:51:06 -0300

    Se crea Clase Banda y clase TipoMusical

---
+++ TP1_L5_GRUPO_8/src/Dominio/Banda.java
@@ -0,0 +1,37 @@
+package Dominio;
+
+public class Banda {
+    private int ID;
+    private String nombre;
+    private TipoMusical tipoMusical;
+
+    public Banda(int ID, String nombre, TipoMusical tipoMusical) {
+        this.ID = ID;
+        this.nombre = nombre;
+        this.tipoMusical = tipoMusical;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public TipoMusical getTipoMusical() {
+        return tipoMusical;
+    }
+
+    public void setTipoMusical(TipoMusical tipoMusical) {
+        this.tipoMusical = tipoMusical;
+    }
+}
---
+++ TP1_L5_GRUPO_8/src/Dominio/TipoMusical.java
@@ -0,0 +1,17 @@
+package Dominio;
+
+public class TipoMusical {
+    private String descripcion;
+
+    public TipoMusical(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}

73a6bfcd4d572726cf45535672cb501477799a65
tfdace <58187350+tfdace@users.noreply.github.com>
2024-03-31 15:27:14 -0300

    se modifica clase Actor.java

--- TP1_L5_GRUPO_8/src/Dominio/Actor.java
+++ TP1_L5_GRUPO_8/src/Dominio/Actor.java
@@ -2,19 +2,23 @@ package Dominio;
 
 public class Actor {
 	
-	private int ID;
+    private int ID;
     private String nombre;
 
-	public int getID() {
-		return ID;
-	}
-	public void setID(int iD) {
-		this.ID = iD;
-	}
+    public Actor(int ID, String nombre) {
+        this.ID = ID;
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+	return ID;
+    }	
+    public void setID(int iD) {
+	this.ID = iD;
+    }
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }

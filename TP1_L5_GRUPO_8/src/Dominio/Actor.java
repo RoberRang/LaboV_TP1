1e53759988591be11620c7ac96934cfb4dd534c7
tfdace <58187350+tfdace@users.noreply.github.com>
2024-03-31 15:23:53 -0300

    se agrega clase Actor.java

---
+++ TP1_L5_GRUPO_8/src/Dominio/Actor.java
@@ -0,0 +1,22 @@
+package Dominio;
+
+public class Actor {
+	
+	private int ID;
+    private String nombre;
+
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		this.ID = iD;
+	}
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}

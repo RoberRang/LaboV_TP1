3e8e0c44733ea314625889fd4b63d68884b9db2c
Roberto Rangogni <rob_rang@hotmail.com>
2024-03-31 15:08:23 -0300

    Sumo el Main

---
+++ TP1_L5_GRUPO_8/src/Dominio/main.java
@@ -0,0 +1,13 @@
+package Dominio;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		EDeporte eDepo = new EDeporte("Futbo1", "03/04/2024", "20", 3, Deporte.FUTBOL, true);
+		System.out.println( eDepo.toString());
+
+	}
+
+}
